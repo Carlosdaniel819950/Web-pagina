74e49a43b40fb55aa6015ae9318c48364da8c819
Carlosdaniel819950 <karllusdanielpaluo99@gmail.com>
2025-05-10 11:05:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const email = localStorage.getItem('userEmail');
+const nome = localStorage.getItem('userName');
+
+
+if (!email || !nome) {
+ 
+  window.location.href = "index.html";
+} else {
+ 
+  document.getElementById('user-email').textContent = email;
+  document.getElementById('user-nome').textContent = nome;
+}
+
+
+document.getElementById('logout-btn').addEventListener('click', () => {
+
+  localStorage.removeItem('userEmail');
+  localStorage.removeItem('userName');
+
+
+  window.location.href = "index.html";
+});
